602ca1f92a842cb67b798711a834c87149fa8c3f
Dan Kozitza <dankoz@gmx.us>
2015-01-25 17:56:52 -0600

    finished coverage

--- unmarshal_test.go
+++ unmarshal_test.go
@@ -2,71 +2,160 @@ package requests
 
 import (
 	"bytes"
-	"testing"
 	"net/http"
+	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-type Emote string
+type emote string
 
-func (e *Emote) Receive(stoic interface{}) error {
-	*e = Emote(stoic.(string) + "! :D")
+func (e *emote) Receive(stoic interface{}) error {
+	s := new(string)
+	*s = stoic.(string) + "! :D"
+	*e = emote(*s)
 	return nil
 }
 
-func TestReceiver(t *testing.T) {
-	var greeting Emote
-	greeting.Receive("hello")
-
-	assert.Equal(t, greeting, "hello! :D")
+type emoteTyper struct {
+	emote
 }
+
+func (et *emoteTyper) ReceiveType() interface{} {
+	return string("")
+}
+
+type structType struct{}
 
 type testTarget struct {
-    Foo int64
-    Bar float64
-    Baz string
-	 Qux *Emote
+	structType
+	Foo                 int64
+	Bar                 float64
+	Baz                 string
+	Qux                 *emote
+	Quxtyper            *emoteTyper
+	Interfacemember     interface{}
+	Defaultermember     interface{} `request:"interfacemember,default"`
+	Ignoredmember       interface{} `request:"-"`
+	structmember        struct{ m int64 }
+	unexported          int64
+	unexportedinterface interface{}
 }
 
-func createTestTarget(request *http.Request) (*testTarget, error) {
-    target := new(testTarget)
-    if err := New(request).Unmarshal(target); err != nil {
-        if inputErrs, ok := err.(InputErrors); ok {
-            // inputErrs is a map of input names to error
-            // messages, so send them to a function to turn
-            // them into a proper user-friendly error message.
-            return nil, inputErrs
-        }
-        return nil, err
-    }
-    return target, nil
+func (tt *testTarget) SetUnexported(i int64) {
+	tt.unexported = i
+}
+func (tt *testTarget) Unexported() int64 {
+	return tt.unexported
 }
 
-func TestUnmarshal_UrlEncoded(t *testing.T) {
-	body := bytes.NewBufferString(`foo=1&bar=2.7&baz=taz&qux=welcome`)
+func (tt *testTarget) SetUnexportedinterface(i interface{}) {
+	tt.unexportedinterface = i
+}
+func (tt *testTarget) Unexportedinterface() interface{} {
+	return tt.unexportedinterface
+}
+
+func (tt *testTarget) SetStructmember(s struct{ m int64 }) {
+	tt.structmember = s
+}
+func (tt *testTarget) Structmember() struct{ m int64 } {
+	return tt.structmember
+}
+
+type testTargetPtr struct {
+	*structType
+}
+
+type testErrTarget struct {
+	noset struct{}
+	noget struct{}
+}
+
+func (tt *testErrTarget) Noset() struct{} {
+	return tt.noset
+}
+func (tt *testErrTarget) SetNoget(s struct{}) {
+	tt.noget = s
+}
+
+type testUnmarshaller struct{}
+
+func (tu *testUnmarshaller) Unmarshal(b interface{}) error {
+	return nil
+}
+
+func TestUnmarshal_All(t *testing.T) {
+	body := bytes.NewBufferString(
+		`foo=1&bar=2.7&baz=taz&quxtyper=welcome&interfacemember=2.7&unexported=1`)
 	httpRequest, err := http.NewRequest("POST", "/", body)
 	httpRequest.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 	require.NoError(t, err)
-	example, err := createTestTarget(httpRequest)
+
+	target := new(testTarget)
+	target.Qux = new(emote)
+	target.Qux.Receive("default")
+
+	err = New(httpRequest).Unmarshal(target)
 	require.NoError(t, err)
 
-	var expectEmote Emote
-	expectEmote.Receive("hello")
-
-	assert.Equal(t, int64(1), example.Foo)
-	assert.Equal(t, float64(2.7), example.Bar)
-	assert.Equal(t, "taz", example.Baz)
-	assert.Equal(t, "welcome! :D", string(*example.Qux))
+	assert.Equal(t, int64(1), target.Foo)
+	assert.Equal(t, float64(2.7), target.Bar)
+	assert.Equal(t, "taz", target.Baz)
+	assert.Equal(t, "default! :D", string(*target.Qux))
+	assert.Equal(t, "welcome! :D", string((*target.Quxtyper).emote))
 }
 
-func TestUnmarshal_Errors(t *testing.T) {
-	body := bytes.NewBufferString(`foo=1&bar=2.7&baz=taz&qux=welcome`)
+func TestUnmarshal_AnonPtr(t *testing.T) {
+	httpRequest, err := http.NewRequest("POST", "/", nil)
+	require.NoError(t, err)
+
+	target := new(testTargetPtr)
+
+	err = New(httpRequest).Unmarshal(target)
+	require.NoError(t, err)
+}
+
+func TestUnmarshalReplace_All(t *testing.T) {
+	body := bytes.NewBufferString(`quxtyper=welcome`)
+	httpRequest, err := http.NewRequest("POST", "/", body)
+	httpRequest.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	require.NoError(t, err)
+
+	target := new(testTarget)
+	target.Foo = 1
+	target.Bar = 2.7
+	target.Baz = "default"
+	target.Qux = new(emote)
+	target.Qux.Receive("default")
+
+	err = New(httpRequest).UnmarshalReplace(target)
+	require.NoError(t, err)
+
+	assert.Equal(t, int64(0), target.Foo)
+	assert.Equal(t, float64(0), target.Bar)
+	assert.Equal(t, "", target.Baz)
+	assert.Equal(t, (*emote)(nil), target.Qux)
+	assert.Equal(t, "welcome! :D", string((*target.Quxtyper).emote))
+}
+
+func TestUnmarshaller(t *testing.T) {
+	body := bytes.NewBufferString(`foo=1`)
 	httpRequest, _ := http.NewRequest("POST", "/", body)
 	httpRequest.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 
-	var target Unmarshaller
-	err := New(httpRequest).Unmarshal(target)
+	require.Implements(t, (*Unmarshaller)(nil), new(testUnmarshaller))
+	var target1 testUnmarshaller
+	err := New(httpRequest).Unmarshal(&target1)
+	assert.NoError(t, err)
+}
+
+func TestUnmarshal_Errors(t *testing.T) {
+	httpRequest, _ := http.NewRequest("POST", "/", nil)
+	httpRequest.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	target1 := new(testErrTarget)
+	err := New(httpRequest).Unmarshal(target1)
 	assert.Error(t, err)
 }
